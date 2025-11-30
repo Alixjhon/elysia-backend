5fb90960bb4cec6e86555671c7b22326ea72500e
Alixjhon <alixjhonescoba@gmail.com>
2025-12-01 02:22:36 +0800

    Ready to deploy backend

--- db.ts
+++ db.ts
@@ -3,6 +3,9 @@ import "dotenv/config";
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Neon
+  },
 });
 
 await client.connect();
